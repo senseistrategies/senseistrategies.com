5f41d653b47ac698406b72c158830f4c8336cc11
senseistrategies <senseistrategies@gmail.com>
2017-11-14 12:36:48 -0500

    added diet calculator html and css

---
+++ public_html/js/diet_calculator.js
@@ -0,0 +1,9 @@
+//Module for contianment of diet calculator.
+DIET_CALCULATOR = {
+    
+    //Define a closure to return the diet calculator master function.
+    retrieveDietCalculatorFunction: function() {
+        
+    }
+    
+};
